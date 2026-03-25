acfabbef02cddf98e70baff352e65696198e3918
RafaelColin <rafael_colin@estudante.sesisenai.org.br>
2026-03-25 10:02:07 -0300

    add - ex3

--- scripts/ex3.js
+++ scripts/ex3.js
@@ -14,4 +14,21 @@ let soma_ganho = 0;
     alert("O ganho anual foi de: "+soma_ganho);
 
     let soma_gasto = 0;
+    for(let i=1; i<=12; i+=1){
+       let gasto = parseFloat(prompt("Digite o gasto do mês "+ i));
+
+       if ( !isNaN(gasto) ){
+            soma_gasto += gasto;
+           console.log(soma);
+        } else{
+            alert("Por favor, digite um numero válido!");
+        }
+        
+    }
+    alert("O gasto anual anual foi de: "+soma_ganho);
+
+    let saldo = soma_ganho - soma_gasto;
+    alert("O saldo anual foi de: "+saldo);
+
+    let soma_gasto = 0;
 }
